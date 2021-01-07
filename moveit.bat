06e880e527aee12cf16a2eca879cb66517342654
James Buck <jbuck2005@hotmail.com>
2021-01-07 10:05:54 -0500

    update directories

--- moveit.bat
+++ moveit.bat
@@ -1,3 +1,3 @@
 del C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.elf
-copy C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin "C:\Users\james\Documents\GitHub\Marlin\firmware archive\paul"
-move C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin "C:\Users\james\Documents\3d\firmware\dad\firmware"
+copy C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin "C:\Users\james\Documents\GitHub\Marlin\firmware\paul"
+move C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin "C:\Users\james\Documents\3d\firmware\paul"
