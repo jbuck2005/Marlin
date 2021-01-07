af348dab47abe6e317150358970ae77b5e991d48
James Buck <jbuck2005@hotmail.com>
2021-01-07 10:27:41 -0500

    correct directory

--- moveit.bat
+++ moveit.bat
@@ -1,3 +1,3 @@
 del C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.elf
-copy C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin "C:\Users\james\Documents\GitHub\Marlin\firmware\james"
+copy C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin "C:\Users\james\Documents\GitHub\Marlin_Custom\firmware\james"
 move C:\Users\james\Documents\3d\firmware\james\Marlin\.pio\build\STM32F103RET6_creality\*.bin "C:\Users\james\Documents\3d\firmware\james"
