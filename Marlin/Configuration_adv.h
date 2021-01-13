f4eff16aea2c2b43d3dda3d2a07c9bd721dab179
James Buck <jbuck2005@hotmail.com>
2021-01-13 09:10:06 -0500

    update USER_GCODE_2

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3352,7 +3352,7 @@
   #define USER_GCODE_1 "M303 C10 E0 S205 U1\nG4 S1\nM303 C10 E-1 S60 U1" // 20210113 added 1s pause for testing (to see if this will resolve issue of beeping during bed PID)
 
   #define USER_DESC_2 "Extrude 100mm filament"
-  #define USER_GCODE_2 "G21\nG90\nG92 E0\nM109 S205\nG1 E100 F300\nG92 E0\nG4 S30\nM104 S0" // 20210113 updated script
+  #define USER_GCODE_2 "G21\nG90\nG92 E0\nM109 S205\nG1 E100 F300\nG92 E0\nG4 S15\nM104 S0" // 20210113 updated script
 
   #define USER_DESC_3 "Probe bed (5m heat time)"  // 20200102 last updated
   #define USER_GCODE_3 "M118 E1 Starting bed probing routine\nM190 S60\nM117 Heating bed\nM117 Waiting 5 minutes for bed to heat evenly\nM118 E1 Waiting 5 minutes for bed to head\nG4 S300\nG28\nM117 Probing bed\nM118 E1 Probing bed\nG29 P1\nM104 S0\nG29 P3\nG29 F10\nG29 S1\nG29 A\nG29 L1\nM500\nM190 S0"
