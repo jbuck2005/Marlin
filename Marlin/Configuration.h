c01bb9fc48f11aab5c938a41ecec088bdbde50fe
james <james@hephaestus.thebucks.ca>
2021-01-13 14:57:13 -0500

    update #define Z_PROBE_LOW_POINT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1071,7 +1071,7 @@
 #define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
 
-#define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
+#define Z_PROBE_LOW_POINT          -5 // Farthest distance below the trigger-point to go before stopping //20210113 changed from -2 to -5; seems to resolve firmware panic on probing
 
 // For M851 give a range for adjusting the Z probe offset
 #define Z_PROBE_OFFSET_RANGE_MIN -3 // 20210105 changed from default -10 to -3 --------------------------------------------------------------------------------------------------------
