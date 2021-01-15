de74a581a346b4fb63e2df41d0665ea7723a0b89
James Buck <jbuck2005@hotmail.com>
2021-01-15 09:05:26 -0500

    increase GRID_MAX_POINTS_X

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1399,7 +1399,7 @@ M500
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
   #define MESH_INSET 1              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 6       // Don't use more than 15 points per axis, implementation limited. // 20210104 reduced from default 10 -----------------------------
+  #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited. // 20210115 set back to default of 10 -----------------------------
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
