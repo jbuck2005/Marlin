4c78fb8935ec851376bcb7eb770a5d32aa5ebc02
james <jbuck2005@hotmail.com>
2021-01-17 17:16:26 -0500

    remove scripts

--- update_marlin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git -C /home/james/Marlin_Custom fetch upstream
-git -C /home/james/Marlin_Custom pull upstream bugfix-2.0.x 
-#git -C /home/james/Marlin_Custom/ pull
-#git -C /home/james/Marlin_Custom/ add /home/james/Marlin_Custom/firmware/james/*.bin
-#git -C /home/james/Marlin_Custom/ commit -m "firmware scheduled build $(date +'%Y%m%d %H:%M:%S')"
-#git -C /home/james/Marlin_Custom/ push 
