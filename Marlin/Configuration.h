f607c206cdfef8cf1d4ca9c919b6d96d04ab1bed
james <jbuck2005@hotmail.com>
2021-02-13 15:16:49 -0500

    firmware (james cr10max) scheduled build 20210213-151447

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -110,7 +110,7 @@
  * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT_2 3 // 20210213 was -1 - set to 3 for pi -> SKR via RX3/TX3 -----------------------------------------------------------------------------------------------------------------------
+#define SERIAL_PORT_2 6 // 20210213 was -1 - set to 3 for pi -> SKR via RX3/TX3 -----------------------------------------------------------------------------------------------------------------------
 
 /**
  * This setting determines the communication speed of the printer.
