3da5064289c412ab34030a142b0938bfb8a6c216
james <jbuck2005@hotmail.com>
2021-02-14 15:23:36 -0500

    firmware (james cr10max) interim build 20210214-152152

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -647,8 +647,8 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-#define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+#define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop. // 20210214 changed from false to true ------------------------------------------------------------------------
+#define Y_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop. // 20210214 changed from false to true ------------------------------------------------------------------------
 #define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
