cf570cb4ab080a80c354b190373072340818a63c
james <jbuck2005@hotmail.com>
2021-02-14 15:37:25 -0500

    firmware (james cr10max) interim build 20210214-153714

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -615,7 +615,7 @@
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
 #define USE_YMIN_PLUG
-#define USE_ZMIN_PLUG // 20210213 diabled as nothing is plugged in - may need to re-enable for filament sensing (sensor is plugged into E0 (3v3 GND PE15) - 20210214 reenabled in attempt to restore comms ------
+//#define USE_ZMIN_PLUG // 20210213 diabled as nothing is plugged in - may need to re-enable for filament sensing (sensor is plugged into E0 (3v3 GND PE15) - 20210214 disabled again to test ------
 //#define USE_XMAX_PLUG
 //#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
@@ -647,8 +647,8 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-#define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop. // 20210214 changed from false to true ------------------------------------------------------------------------
-#define Y_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop. // 20210214 changed from false to true ------------------------------------------------------------------------
+#define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop. // 20210214 endstop pins are PE15 and GND - tested and works in "normal" mode ---------------------------------
+#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop. // 20210214 endstop pins are PE10 and GND - tested and works in "normal" mode ---------------------------------
 #define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
