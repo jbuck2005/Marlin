ee76e2ae8f2c0819938831950134848f227c4826
james <jbuck2005@hotmail.com>
2021-02-15 18:50:46 -0500

    firmware (james cr10max) interim build 20210215-184853

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1186,8 +1186,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 450 // 20210215 changed from 200 to 450 --------------------------------------------------------------------------------------------------------------------------------------------
+#define Y_BED_SIZE 450 // 20210215 changed from 200 to 450 --------------------------------------------------------------------------------------------------------------------------------------------
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1195,7 +1195,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 470 // 20210215 changed from 200 to 450 ---------------------------------------------------------------------------------------------------------------------------------------------
 
 /**
  * Software Endstops
