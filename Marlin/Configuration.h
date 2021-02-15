f77a5f3a547a92cf320c3cbf1942fbb6ac24a134
james <jbuck2005@hotmail.com>
2021-02-15 14:01:28 -0500

    firmware (james cr10max) interim build 20210215-140117

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2568,7 +2568,7 @@ M500
 #undef FAN_COUNT
 #define FAN_COUNT       3       // 20210215 added definition to allow all 3 fans
 
-#define NUM_M106_FANS 3 // 20210215 changed from 1 to 3 -----------------------------------------------------------------------------------------------------------------------------------------------
+#define NUM_M106_FANS 2 // 20210215 changed from 1 to 3 -----------------------------------------------------------------------------------------------------------------------------------------------
 
 // Increase the FAN PWM frequency. Removes the PWM noise but increases heating in the FET/Arduino
 //#define FAST_PWM_FAN
