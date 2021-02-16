8c3d885765ec78fe41b5da1f8645c436cad4cc18
james <jbuck2005@hotmail.com>
2021-02-16 06:17:04 -0500

    firmware (james cr10max) interim build 20210216-061457

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -480,7 +480,7 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN	PE5  // 20210216 changed from -1 to FAN1_PIN (PE5) which is hooked up to the hot-end fan --------------------------------------------------------------------------------------
+//#define E0_AUTO_FAN_PIN	PE5  // 20210216 changed from -1 to FAN1_PIN (PE5) which is hooked up to the hot-end fan --------------------------------------------------------------------------------------
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
