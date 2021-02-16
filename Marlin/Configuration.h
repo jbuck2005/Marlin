5f892cda68fa14e89f715514e257fb17b94ca599
james <jbuck2005@hotmail.com>
2021-02-16 14:01:28 -0500

    firmware (james cr10max) interim build 20210216-135920

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2584,7 +2584,7 @@ M500
 //#undef FAN_COUNT
 //#define FAN_COUNT       1       // 20210215 added definition to allow all 3 fans --------------------------------------------------------------------------------------------------------------------
 
-#define NUM_M106_FANS 1 // 20210216 changed from 1 to 0 - unsuccessful; will need to find where to add FAN_COUNT --------------------------------------------------------------------------------------
+#define NUM_M106_FANS 3 // 20210216 changed from 1 to 0 - unsuccessful; will need to find where to add FAN_COUNT --------------------------------------------------------------------------------------
 
 // Increase the FAN PWM frequency. Removes the PWM noise but increases heating in the FET/Arduino
 //#define FAST_PWM_FAN
@@ -2592,7 +2592,7 @@ M500
 // Use software PWM to drive the fan, as for the heaters. This uses a very low frequency
 // which is not as annoying as with the hardware PWM. On the other hand, if this frequency
 // is too low, you should also increment SOFT_PWM_SCALE.
-//#define FAN_SOFT_PWM // 20210215 enabled --------------------------------------------------------------------------------------------------------------------------------------------------------------
+#define FAN_SOFT_PWM // 20210215 enabled --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Incrementing this by 1 will double the software PWM frequency,
 // affecting heaters, and the fan if FAN_SOFT_PWM is enabled.
