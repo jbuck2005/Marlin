f237859d7640814ce076c367a76ececc61908a05
james <jbuck2005@hotmail.com>
2021-02-19 06:07:06 -0500

    firmware (james cr10max) interim build 20210219-060424

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2579,9 +2579,6 @@ M500
   #define E0_AUTO_FAN_PIN               FAN1_PIN
   #endif
 
-	NOTE: FAN pin and E0_AUTO_FAN_PIN declarations can allegedly be redefined here if needed because Configuration.h is processed before pins_BTT_SKR_PRO_common.h
-		- 20210218 tested this and was NOT successful
-
 	Board layout w/ pin names:
 	------------------------------------------------------------------------------------
  		  board names:	FAN0			FAN1			FAN2
@@ -2590,13 +2587,8 @@ M500
 	intended control mode:	CONTROLLER_FAN_PIN	E0_AUTO_FAN_PIN		M106
 	   Marlin definitions:	FAN3			FAN1			FAN2
 
-	FAN_PIN was previouslt redefined as -1 in ./Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h
-
 *///	-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// 20210218 - tried to redefine FAN pins here but failed to compile so I had to make changes to ./Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h - namely set FAN_PIN to -1 ------------------------
-//#undef FAN_PIN         // 20210219 need to undefine before re-defining FAN pin to prevent compile time warnings ---------------------------------------------------------------------------------------
-//#define FAN_PIN -1     // 20210219 define FAN_PIN as -1 to allow PC8 (FAN0 on SKR) to be re-purposed as CONTROLLER_FAN_PIN under the pin name FAN3 for stepper motor cooling --------------------------
      // FAN1_PIN PE5      20210219 Fan1 - E0_AUTO_FAN_PIN	- extruder cooling fan		- FAN1 on SKR PRO v1.2 --------------------------------------------------------------------------------
      // FAN2_PIN PE6      20210219 Fan2 - M106			- part cooling fan		- FAN2 on SKR PRO v1.2 --------------------------------------------------------------------------------
 #define FAN3_PIN PC8   // 20210219 Fan3 - CONTROLLER_FAN_PIN	- stepper motor cooling fan     - FAN0 on SKR PRO v1.2 --------------------------------------------------------------------------------
@@ -2611,7 +2603,7 @@ M500
 // Use software PWM to drive the fan, as for the heaters. This uses a very low frequency
 // which is not as annoying as with the hardware PWM. On the other hand, if this frequency
 // is too low, you should also increment SOFT_PWM_SCALE.
-//#define FAN_SOFT_PWM // 20210215 enabled --------------------------------------------------------------------------------------------------------------------------------------------------------------
+#define FAN_SOFT_PWM // 20210215 enabled --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Incrementing this by 1 will double the software PWM frequency,
 // affecting heaters, and the fan if FAN_SOFT_PWM is enabled.
