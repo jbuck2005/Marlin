e7a9f5c36e880ef0226c0823ea8ca08b636534c5
james <jbuck2005@hotmail.com>
2021-02-19 05:38:49 -0500

    firmware (james cr10max) interim build 20210219-053834

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2596,6 +2596,8 @@ M500
 
 // 20210218 - tried to redefine FAN pins here but failed to compile so I had to make changes to ./Marlin/src/pins/stm32f4/pins_BTT_SKR_PRO_common.h - namely set FAN_PIN to -1 ------------------------
 //#define FAN3_PIN	PC8 // 20210218 - PC8 = FAN0 on SKR PRO board - CONTROLLER_FAN_PIN - this is wired to the stepper motor cooling fan -----------------------------------------------------------
+#define FAN_PIN -1     // Fan -
+#define FAN3_PIN PC8   // Fan3 - CONTROLLER_FAN_PIN  - stepper motor cooling fan     - FAN0
 
 // Set number of user-controlled fans. Disable to use all board-defined fans.
 // :[1,2,3,4,5,6,7,8]
