913f01e07de44166ddc2fa385719935e1920016d
james <jbuck2005@hotmail.com>
2021-02-26 15:26:49 -0500

    firmware (john) scheduled build 20210226 15:26:49

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1681,23 +1681,14 @@ M500
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 195 // 20200911 was 220 ----------------------------------------------------------------------------------------------------------------------------
 #define PREHEAT_1_TEMP_BED     55 // 20200911 was 45 -----------------------------------------------------------------------------------------------------------------------------
+#define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED   0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED     60
-#define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
-
-#define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED     70
-#define PREHEAT_1_TEMP_CHAMBER 35
-#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
-
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
 #define PREHEAT_2_TEMP_CHAMBER 35
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
 
 /**
  * Nozzle Park
